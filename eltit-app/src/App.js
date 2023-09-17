188c359678902488855d3ce6627cf3619bdbca1c
arielgodoy <arielgodoy@gmail.com>
2023-09-16 21:22:43 -0300

    instala bootstrap 5

---
+++ eltit-app/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
